5390f8fe8a8e4248685cdc83076d268ef2c1af71
Ganesh <121486938+Ganesh-G0@users.noreply.github.com>
2024-04-14 16:04:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,51 +1,66 @@
 import confetti from 'https://cdn.skypack.dev/canvas-confetti';
 
 const game = ["Rock", "Paper", "Scissor"];
-let botsChoice = game[(Math.floor(Math.random() * game.length))];
-// console.log(botsChoice);
-const rock = document.querySelector("#rock");
-const paper = document.querySelector("#paper");
-const scissor = document.querySelector("#scissor");
-const result = document.querySelector("#result");
-result.style.display = "none";
-const playersPick = document.querySelector("#playersPick");
-const botsPick = document.querySelector("#botsPick");
-const won = document.querySelector("#won");
-let playersChoice;
-rock.onclick = updateRock;
-paper.onclick = updatePaper;
-scissor.onclick = updateScissor;
 
-function updateRock() { playersChoice = game[0]; winner(); }
-function updatePaper() { playersChoice = game[1]; winner(); }
-function updateScissor() { playersChoice = game[2]; winner(); }
+$("#result").hide(); $("#score").hide();
+
+let b1Score = 0;
+let p1Score = 0; 
+$("#botScore").text(b1Score); 
+$("#playerScore").text(p1Score); 
+
+
+let playersChoice; 
+$("#rock").click(updateRock);
+$("#paper").click(updatePaper);
+$("#scissor").click(updateScissor);
+function updateRock() { playersChoice = game[0]; };
+function updatePaper() { playersChoice = game[1]; };
+function updateScissor() { playersChoice = game[2]; };
 
 function winner() {
-  if (playersChoice == botsChoice) {
-    console.log("Draw");
-    result.style.display = "block";
-    playersPick.innerText = playersChoice;
-    botsPick.innerText = botsChoice;
-    won.innerText = "It's a Draw!"; 
-    botsChoice = game[(Math.floor(Math.random() * game.length))]   
-  } 
-  else if ( playersChoice == game[0] && botsChoice == game[1] || 
+  const botsChoice = game[Math.floor(Math.random() * game.length)];
+  if ( playersChoice == game[1] && botsChoice == game[0] || 
+    playersChoice == game[2] && botsChoice == game[1] || 
+    playersChoice == game[0] && botsChoice == game[2] ) {
+      p1Score++;
+      $("#playerScore").text(p1Score); $("#botScore").text(b1Score); 
+      $("#playersPick").text(playersChoice); $("#botsPick").text(botsChoice);
+      $("#winnerIs").html("&#128128; You Won! &#127881;");confetti();
+    }
+    else if ( playersChoice == game[0] && botsChoice == game[1] || 
     playersChoice == game[1] && botsChoice == game[2] || 
     playersChoice == game[2] && botsChoice == game[0] ) {
-    console.log("Bot Wins");
-    result.style.display = "block";
-    playersPick.innerText = playersChoice;
-    botsPick.innerText = botsChoice;
-    won.innerHTML = "&#129302; Bot Won! &#127881;";    
-    botsChoice = game[(Math.floor(Math.random() * game.length))]
+      b1Score++;
+      $("#playerScore").text(p1Score); $("#botScore").text(b1Score); 
+      $("#playersPick").text(playersChoice); $("#botsPick").text(botsChoice);
+      $("#winnerIs").html("&#129302; Bot Won! &#127881;");    
   } 
-  else { 
-    console.log("Player Wins"); 
-    result.style.display = "block";
-    playersPick.innerText = playersChoice;
-    botsPick.innerText = botsChoice;
-    won.innerHTML = "&#128128; You Won! &#127881;";  
-    botsChoice = game[(Math.floor(Math.random() * game.length))]
-    confetti();
+  else {
+    $("#playerScore").text(p1Score); $("#botScore").text(b1Score); 
+    $("#playersPick").text(playersChoice); $("#botsPick").text(botsChoice);
+    $("#winnerIs").text("It's a Draw!");
+  } 
+  console.log("botsChoice: ", botsChoice, "your Choice: ", playersChoice) ;
+  console.log("bots Wins : ", b1Score, "players Wins : ", p1Score);
 }
-}
+
+$('.btn-group').click(() => { winner(); $('#score').show(); $("#result").show(); })
+
+$("#toggle").click(() => {
+ if ($("body").hasClass("dark-mode")) {
+   $('body').removeClass('dark-mode')
+   $('body').addClass('light-mode')
+  } else {
+    $('body').removeClass('light-mode')
+    $('body').addClass('dark-mode')    
+ }
+  if ($('body').hasClass('dark-mode')) { 
+    $(".gg-moon").addClass("d-none");
+    $(".gg-sun").removeClass("d-none");
+  } else {
+    $(".gg-sun").addClass("d-none");
+    $(".gg-moon").removeClass("d-none");
+  }
+
+})
